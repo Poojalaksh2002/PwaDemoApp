9f93a3b0a5b8459f1f89de3122cca8a678a9adf0
Pooja Beyond <pooja@beyondfinity.com>
2023-07-12 13:08:57 +0530

    Asset url changed

--- public/worker.js
+++ public/worker.js
@@ -1,5 +1,19 @@
 var CACHE_NAME = "pwa-task-manager";
-var urlsToCache = ["/", "/completed"];
+var urlsToCache = [
+  "/PwaDemoApp/static/css/main.073c9b0a.css",
+  "/PwaDemoApp/static/css/main.073c9b0a.css.map",
+  "/PwaDemoApp/static/js/main.46c23252.js",
+  "/PwaDemoApp/static/js/main.46c23252.js.LICENSE.txt",
+  "/PwaDemoApp/static/js/main.46c23252.js.map",
+  "/PwaDemoApp/static/media/logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg",
+  // "/",
+  // "./main.073c9b0a.css",
+  // "./main.46c23252.js",
+  // "./logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg",
+  // "./index.html",
+  // "./main.073c9b0a.css.map",
+  // "./main.46c23252.js.map",
+];
 
 // Install a service worker
 self.addEventListener("install", (event) => {
@@ -14,17 +28,36 @@ self.addEventListener("install", (event) => {
 // Cache and return requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
+    caches.match(event.request).then((cacheRes) => {
+      // If the file is not present in STATIC_CACHE,
+      // it will be searched in DYNAMIC_CACHE
+      return (
+        cacheRes ||
+        fetch(event.request).then((fetchRes) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
     })
   );
 });
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       // Cache hit - return response
+//       if (response) {
+//         console.log("fetched Success");
+//         return response;
+//       }
+//       return fetch(event.request);
+//     })
+//   );
+// });
 // Update a service worker
 self.addEventListener("activate", (event) => {
+  console.log("activated");
   var cacheWhitelist = ["pwa-task-manager"];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
